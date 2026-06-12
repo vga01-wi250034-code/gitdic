5eeb6799336179b8d894c391eb108d72aa75ab84
sagun11-19 <vga01-wi250043@sankogakuen.jp>
2026-06-12 15:40:39 +0900

    commit

--- history.js
+++ history.js
@@ -1,5 +1,11 @@
 // ⚠️ 各ターンの終わりに、画面に表示された自分の結果コードをこの配列の中に追加してください。
 const gameHistory = [
     // ここから下に追記していく
-
+{ turn: 1, player: "playerB", finalPos: "茅ヶ崎", event: "無事に移動した！", foundTreasure: false },
+{ turn: 2, player: "playerB", finalPos: "相模湖", event: "無事に移動した！", foundTreasure: false },
+{ turn: 3, player: "playerB", finalPos: "厚木", event: "無事に移動した！", foundTreasure: false },
+{ turn: 4, player: "playerB", finalPos: "三崎口", event: "無事に移動した！", foundTreasure: false },
+{ turn: 5, player: "playerB", finalPos: "藤沢", event: "無事に移動した！", foundTreasure: false },
+{ turn: 6, player: "playerB", finalPos: "箱根湯本", event: "無事に移動した！", foundTreasure: false },
+{ turn: 7, player: "playerB", finalPos: "小田原", event: "💡 ヒント発見！ 古文書：「東の海を目指す手前、北へと進路を変える分岐点にそれはある」", foundTreasure: false },
 ];
