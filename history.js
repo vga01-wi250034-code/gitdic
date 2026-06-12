e309757cb79a36360b2bc4974c8e08d2a752b6a2
user.email <vga01-wi250034@sankogakuen.jp>
2026-06-12 15:39:20 +0900

    Commit

--- history.js
+++ history.js
@@ -1,5 +1,10 @@
 // ⚠️ 各ターンの終わりに、画面に表示された自分の結果コードをこの配列の中に追加してください。
 const gameHistory = [
     // ここから下に追記していく
+    { turn: 1, player: "playerA", finalPos: "三崎口", event: "無事に移動した！", foundTreasure: false },
+    { turn: 1, player: "playerA", finalPos: "鎌倉", event: "無事に移動した！", foundTreasure: false },
+    { turn: 1, player: "playerA", finalPos: "新百合丘", event: "💡 ヒント発見！ 村人：「南西へ向かい、町と町を繋ぐ大きな交差点となる場所だべ」", foundTreasure: false },
+    { turn: 1, player: "playerA", finalPos: "町田", event: "無事に移動した！", foundTreasure: false },
+    { turn: 1, player: "playerA", finalPos: "新百合丘", event: "💡 ヒント発見！ 村人：「南西へ向かい、町と町を繋ぐ大きな交差点となる場所だべ」", foundTreasure: false },
 
 ];
